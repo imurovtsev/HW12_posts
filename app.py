8314effccf4e786f70c353da74c32fd8af82a1bd
imurovtsev <imurovtsev@gmail.com>
2022-07-14 00:35:28 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+import logging
+
+from flask import Flask, Blueprint, request, render_template, send_from_directory
+
+from loader.views import loader_blueprint
+from main.views import main_blueprint
+
+app = Flask(__name__)
+
+app.register_blueprint(main_blueprint)
+app.register_blueprint(loader_blueprint)
+
+logging.basicConfig(filename="basic.log", level=logging.INFO)
+
+
+@app.route("/uploads/<path:path>")
+def static_dir(path):
+    return send_from_directory("uploads", path)
+
+
+app.run()
+# app.run(debug=True)
